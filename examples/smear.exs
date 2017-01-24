f79141010fc1f334b50a0141e1796039c273d509
Jason Orendorff <jason.orendorff@gmail.com>
2017-01-24 13:08:56 -0600

    An example program.

---
+++ examples/smear.exs
@@ -0,0 +1,16 @@
+# Demo program - Blur a picture 9 times
+
+Blur.open("nashville.jpg")
+|> Stream.iterate(&Blur.blur(&1))
+|> Stream.with_index()
+|> Stream.drop(1)
+|> Stream.take(9)
+|> Stream.map(fn({image, index}) ->
+  Task.async(fn() ->
+    filename = "blurry-#{index}.png"
+    Blur.save(image, filename)
+    #System.cmd("gvfs-open", [filename])
+  end)
+end)
+|> Stream.each(&Task.await(&1, nil))
+|> Enum.to_list()
